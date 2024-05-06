34e1211c2af7af9f04fb77c4ea99589a8b3f50b7
jorge l <jorge.leandro@utec.edu.pe>
2024-05-06 11:19:27 -0500

    some changes on user calendar and task and user dtos (service,repo,controller,class)

---
+++ proyecto_dbp/src/main/java/com/example/proyecto_dbp/User/dto/UserDto.java
@@ -0,0 +1,28 @@
+package com.example.proyecto_dbp.User.dto;
+
+
+public class UserDto {
+    private Long id;
+
+    @NotBlank(message = "Name is required")
+    @Size(min = 2, max = 100, message = "Name must be between 2 and 100 characters")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+
+    public UserDto() {}
+
+    public void setId(Long id) {this.id = id;}
+    public void setName(String name) {this.name = name;}
+    public void setEmail(String email) {this.email = email;}
+
+    public String getName() {return name;}
+    public Long getId() {return id;}
+    public String getEmail() {return email;}
+
+
+
+}
