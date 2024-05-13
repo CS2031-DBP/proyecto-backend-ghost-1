7635a640ea297bcdd172d287fb1eff9fc0a47674
jorge l <jorge.leandro@utec.edu.pe>
2024-05-13 14:07:50 -0500

    Changes by Jose Aguilar

--- src/main/java/com/example/proyecto_dbp/VoiceCommand/application/VoiceCommandController.java
+++ src/main/java/com/example/proyecto_dbp/VoiceCommand/application/VoiceCommandController.java
@@ -1,4 +1,21 @@
 package com.example.proyecto_dbp.VoiceCommand.application;
 
+import com.example.proyecto_dbp.VoiceCommand.domain.VoiceCommand;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.proyecto_dbp.VoiceCommand.domain.VoiceCommandService;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/voiceCommands")
 public class VoiceCommandController {
+    @Autowired
+    private VoiceCommandService voiceCommandService;
+
+    @GetMapping("/user/{userId}")
+    public List<VoiceCommand> getCommandsByUserId(@PathVariable Long userId) {
+        return voiceCommandService.getCommandsByUser(userId);
+    }
+
+    // Más endpoints
 }
--- src/main/java/com/example/proyecto_dbp/VoiceCommand/domain/VoiceCommandService.java
+++ src/main/java/com/example/proyecto_dbp/VoiceCommand/domain/VoiceCommandService.java
@@ -1,4 +1,20 @@
 package com.example.proyecto_dbp.VoiceCommand.domain;
 
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.proyecto_dbp.VoiceCommand.infrastructure.VoiceCommandRepository;
+import com.example.proyecto_dbp.VoiceCommand.domain.VoiceCommand;
+import java.util.List;
+
+
+@Service
 public class VoiceCommandService {
+    @Autowired
+    private VoiceCommandRepository voiceCommandRepository;
+
+    public List<VoiceCommand> getCommandsByUser(Long userId) {
+        return voiceCommandRepository.findByUserId(userId);
+    }
+
+    // Más métodos de negocio
 }
