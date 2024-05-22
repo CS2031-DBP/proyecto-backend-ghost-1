baed32bd4e5415efb194fa8a772298a4594ed0b9
jorge l <jorge.leandro@utec.edu.pe>
2024-05-22 13:45:11 -0500

    verificar endpoints para coherencia del proyecto ningun error por ahora (tiene exceptions pero ningun uso por ahora)

---
+++ src/main/java/com/example/proyecto_dbp/exceptions/UnauthorizeOperationException.java
@@ -0,0 +1,5 @@
+package com.example.proyecto_dbp.exceptions;
+
+public class UnauthorizeOperationException extends RuntimeException{
+    public UnauthorizeOperationException(String message) {super(message);}
+}
