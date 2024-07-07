16c0ae7c1b2a58291bc12ed0b776aaec576cf4a3
yoryi <j_leandro_vaskez@hotmail.com>
2024-07-07 06:46:01 -0500

    cors added

--- src/main/java/com/example/proyecto_dbp/ProyectoDbpApplication.java
+++ src/main/java/com/example/proyecto_dbp/ProyectoDbpApplication.java
@@ -1,9 +1,33 @@
 package com.example.proyecto_dbp;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ProyectoDbpApplication {
-	public static void main(String[] args) {SpringApplication.run(ProyectoDbpApplication.class, args);}
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDbpApplication.class, args);
+	}
+
+	@Bean
+	ModelMapper modelMapper() {return new ModelMapper();}
+
+	@Bean
+	public WebMvcConfigurer corsMappingConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "PATCH")
+						.maxAge(3600)
+						.allowedHeaders("*")
+						.allowCredentials(false);
+			};
+		};
+	};
 }
